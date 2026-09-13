333947e688373faabf2c50a4544f4972c8fe71b1
crosseyedCOBRA <crosseyedCOBRA@gmail.com>
2026-09-13 19:20:27 -0400

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,28 +1,19 @@
 { config, pkgs, ... }:
 
-let
-  qtileNoTests = pkgs.qtile.overrideAttrs (oldAttrs: {
-    doCheck = false;
-  });
-in
 {
   imports = [
     ./hardware-configuration.nix
   ];
 
-  # Boot
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Networking
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  # Time / locale
   time.timeZone = "America/New_York";
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # X11 + LightDM + Qtile
   services.xserver = {
     enable = true;
 
@@ -30,20 +21,16 @@ in
 
     windowManager.qtile = {
       enable = true;
-      package = qtileNoTests;
     };
   };
 
-  # PipeWire
   services.pipewire = {
     enable = true;
     pulse.enable = true;
   };
 
-  # Graphics
   hardware.graphics.enable = true;
 
-  # User
   users.users.mike = {
     isNormalUser = true;
     description = "Mike";
@@ -56,10 +43,8 @@ in
     ];
   };
 
-  # Passwordless sudo
   security.sudo.wheelNeedsPassword = false;
 
-  # Basic utilities
   environment.systemPackages = with pkgs; [
     git
     vim
